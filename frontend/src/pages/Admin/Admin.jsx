a6db93462134bf23967e25bbc0d8941398d5745f
sax59 <waquetv@gmail.com>
2022-12-07 12:42:23 +0100

    Add search bar for user and sugest

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,5 +1,7 @@
 import UserCard from "@components/UserCard/UserCard";
 
+import "./style.scss";
+
 export default function Admin() {
   return (
     <>
@@ -7,7 +9,43 @@ export default function Admin() {
       <div className="validateUser">
         <h2>User to be validated</h2>
         <p>Users awaiting validation</p>
-        <UserCard />
+        <div className="usercard">
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
+      </div>
+      <h2>Management</h2>
+      <div className="management">
+        <div className="usermanagement">
+          <h3>Users management</h3>
+          <form>
+            <label htmlFor="sugestsearch">
+              Search for sugestion:
+              <input type="search" />
+            </label>
+          </form>
+        </div>
+        <div className="sugestmanagement">
+          <h3>Sugestions management</h3>
+          <form>
+            <label htmlFor="sugestsearch">
+              Search for users
+              <input type="search" />
+            </label>
+          </form>
+        </div>
       </div>
     </>
   );
