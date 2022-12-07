8d42ebb17edf898fc7963fb8c2adebeafb3b7a9a
Alexis Foulont <alexisfoulont@gmail.com>
2022-12-07 14:11:18 +0100

    resolved merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
-import SubscriptionForm from "@pages/SubscriptionForm/SubscriptionForm";
-
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <SubscriptionForm />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
