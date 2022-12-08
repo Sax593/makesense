ccbbb5789a0819a93a38d18c18732e508aa4e5cf
guenk <gueguenk@gmail.com>
2022-12-08 17:25:03 +0100

    adding cards

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,10 +4,13 @@ import logoMS from "../../assets/pills.svg";
 import logoMS2 from "../../assets/pills2.svg";
 import logoMS3 from "../../assets/pills3.svg";
 
+import SugestCard from "@components/SugestCard/SugestCard";
+
 export default function Home() {
   return (
     <>
       <Myhome />
+
       <img className="logoxMS" src={logoMS} alt="logoMS" />
       <img className="logoxMS2" src={logoMS2} alt="logoMS" />
       <img className="logoxMS3" src={logoMS3} alt="logoMS" />
@@ -15,6 +18,12 @@ export default function Home() {
       <section className="titlehome">
         <h1 className="settingstitle"></h1>
         <h2 className="titlesuggest">Suggestions</h2>
+        <div className="cardx">
+          <SugestCard />
+          <SugestCard />
+          <SugestCard />
+          <SugestCard />
+        </div>
       </section>
     </>
   );
