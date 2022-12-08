7e154496268131aefd1ad745211986ec54804f5a
guenk <gueguenk@gmail.com>
2022-12-08 15:06:35 +0100

    adding style to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,9 @@ import Home from "@pages/Home/Home";
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <Home />
-      </div>
-      <h1>Suggestions</h1>
-    </>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
