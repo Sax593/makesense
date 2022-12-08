7674b167bbdad2b64f0e1e60f1f58b045ee5fd36
sax59 <waquetv@gmail.com>
2022-12-08 11:01:31 +0100

    Create sugest files

---
+++ frontend/src/assets/creatsugest.js
@@ -0,0 +1,14 @@
+const suggest = [];
+
+for (let i = 1; i < 5; i++) {
+  suggest.push({
+    title: `Suggest ${i}`,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    repercution:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    impacted: [1, 2, 5],
+    expert: [3, 4],
+  });
+}
+console.log(suggest);
