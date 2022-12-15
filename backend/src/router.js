42f2b34c53da858606df5f7df7643bde2dccd724
Fbouchezpro <fbouchezpro@gmail.com>
2022-12-15 15:25:49 +0100

    adding router.post comments

--- backend/src/router.js
+++ backend/src/router.js
@@ -18,4 +18,12 @@ router.post("/users", usersControllers.add);
 router.put("/users/:id", usersControllers.edit);
 router.delete("/users/:id", usersControllers.destroy);
 
+const commentsControllers = require("./controllers/commentsControllers");
+
+router.get("/comments", commentsControllers.browse);
+router.get("/comments/:id", commentsControllers.read);
+router.post("/comments", commentsControllers.add);
+router.put("/comments/:id", commentsControllers.edit);
+router.delete("/comments/:id", commentsControllers.destroy);
+
 module.exports = router;
