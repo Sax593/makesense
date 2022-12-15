252f28806c8734820b1eaf2a3e4bec8a36eaa678
Alexis Foulont <alexisfoulont@gmail.com>
2022-12-15 11:02:24 +0100

    Fix suggestCard and Home

--- frontend/src/components/SuggestDetails/SuggestDetails.jsx
+++ frontend/src/components/SuggestDetails/SuggestDetails.jsx
@@ -1,50 +1,28 @@
-import sugest from "@assets/sugest";
 import "./style.scss";
-import user from "@assets/user";
 
 export default function SuggestDetails() {
   return (
     <section className="suggestdetail">
-      <h2>{sugest[1].title}</h2>
+      <h2>title</h2>
       <div className="main">
         <article className="content">
-          <p className="description">{sugest[1].description}</p>
-          <p className="repercution">{sugest[1].repercution}</p>
+          <p className="description">description</p>
+          <p className="repercution">repercution</p>
         </article>
         <article className="people">
           <div className="impacted">
             <h3 className="peopletitle">Impacted people</h3>
             <div className="avatar">
-              <img
-                className="avatarpicture"
-                src={user[1].picture}
-                alt={`${user[1].lastname} avatar`}
-              />
-              <img
-                className="avatarpicture"
-                src={user[2].picture}
-                alt={`${user[2].lastname} avatar`}
-              />
-              <img
-                className="avatarpicture"
-                src={user[5].picture}
-                alt={`${user[5].lastname} avatar`}
-              />
+              <img className="avatarpicture" src="{user[1].picture}" alt="" />
+              <img className="avatarpicture" src="{user[2].picture}" alt="" />
+              <img className="avatarpicture" src="{user[5].picture}" alt="" />
             </div>
           </div>
           <div className="expert">
             <h3 className="peopletitle">Expert people</h3>
             <div className="avatar">
-              <img
-                className="avatarpicture"
-                src={user[3].picture}
-                alt={`${user[3].lastname} avatar`}
-              />
-              <img
-                className="avatarpicture"
-                src={user[4].picture}
-                alt={`${user[4].lastname} avatar`}
-              />
+              <img className="avatarpicture" src="{user[5].picture}" alt="" />
+              <img className="avatarpicture" src="{user[5].picture}" alt="" />
             </div>
           </div>
         </article>
