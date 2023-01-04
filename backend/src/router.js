54a08c8a4a4aca771609791a6ad18e5889852d5c
Fbouchezpro <fbouchezpro@gmail.com>
2023-01-04 12:03:54 +0100

    remove the deadly code

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,22 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-// const suggestsControllers = require("./controllers/suggestsControllers");
-
-// router.get("/suggests", suggestsControllers.browse);
-// router.get("/suggests/:id", suggestsControllers.read);
-// router.post("/suggests", suggestsControllers.add);
-// router.put("/suggests/:id", suggestsControllers.edit);
-// router.delete("/suggests/:id", suggestsControllers.destroy);
-
-// const usersControllers = require("./controllers/usersControllers");
-
-// router.get("/users", usersControllers.browse);
-// router.get("/users/:id", usersControllers.read);
-// router.post("/users", usersControllers.add);
-// router.put("/users/:id", usersControllers.edit);
-// router.delete("/users/:id", usersControllers.destroy);
-
 const commentsControllers = require("./controllers/commentsControllers");
 
 router.get("/comments", commentsControllers.browse);
