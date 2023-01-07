92c05cceace4ce953f0baa9c8e9a243b7935762b
Sax59 <waquetv@gmail.com>
2023-01-07 14:53:56 +0100

    Change MySpace to Nav and delete unnecessary Link

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import SugestCard from "@components/SugestCard/SugestCard";
-import Myhome from "@components/Myspace/Myspace";
-import "./Style.scss";
+import Nav from "@components/Nav/Nav";
 import logoMS from "../../assets/pills.svg";
 import logoMS2 from "../../assets/pills2.svg";
 import logoMS3 from "../../assets/pills3.svg";
+import "./Style.scss";
 
 export default function Home() {
   return (
     <>
-      <Myhome />
+      <Nav />
 
       <img className="logoxMS" src={logoMS} alt="logoMS" />
       <img className="logoxMS2" src={logoMS2} alt="logoMS" />
