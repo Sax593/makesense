a586aec5fdcd8c3f3be3e629b8fb75b3df4ba1f6
Alexis Foulont <alexisfoulont@gmail.com>
2023-01-09 14:34:34 +0100

    add responsive design

--- frontend/src/components/NavBarSuggest/NavBarSuggest.jsx
+++ frontend/src/components/NavBarSuggest/NavBarSuggest.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import "./style.scss";
+import { MdTopic } from "react-icons/md";
+import { VscCommentDiscussion } from "react-icons/vsc";
+import { FaVoteYea } from "react-icons/fa";
+import { AiFillFileUnknown } from "react-icons/ai";
 
 export default function NavBarSuggest() {
   const [isActive, setIsActive] = useState("idea");
@@ -12,7 +16,10 @@ export default function NavBarSuggest() {
         className={isActive === "idea" ? "active" : "noActive"}
         onClick={() => setIsActive("idea")}
       >
-        <h3 className="text">L'idée</h3>
+        <h3 className="text">The Idea</h3>
+        <div className="iconSuggest">
+          <MdTopic />
+        </div>
       </button>
       <div className="triangle" />
 
@@ -23,6 +30,9 @@ export default function NavBarSuggest() {
         onClick={() => setIsActive("contrib")}
       >
         <h3 className="text">Contributions</h3>
+        <div className="iconSuggest">
+          <VscCommentDiscussion />
+        </div>
       </button>
       <div className="triangle" />
 
@@ -33,6 +43,9 @@ export default function NavBarSuggest() {
         onClick={() => setIsActive("votes")}
       >
         <h3 className="text">Votes</h3>
+        <div className="iconSuggest">
+          <FaVoteYea />
+        </div>
       </button>
       <div className="triangle" />
 
@@ -42,7 +55,10 @@ export default function NavBarSuggest() {
         className={isActive === "finaleD" ? "active" : "noActive"}
         onClick={() => setIsActive("finaleD")}
       >
-        <h3 className="text">Décision Finale</h3>
+        <h3 className="text">Final Decision</h3>
+        <div className="iconSuggest">
+          <AiFillFileUnknown />
+        </div>
       </button>
     </div>
   );
