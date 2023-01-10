9331e23624c7fe90c362e9226dd00250895cbb76
sax59 <waquetv@gmail.com>
2023-01-10 12:03:30 +0100

    remove nav

--- frontend/src/components/FinalDecision/FinalDecision.jsx
+++ frontend/src/components/FinalDecision/FinalDecision.jsx
@@ -1,17 +1,13 @@
 import FinalContent from "@components/FinalContent/FinalContent";
-import Nav from "@components/Nav/Nav";
 import ResultVote from "@components/ResultVote/ResultVote";
 
 import "./style.scss";
 
 export default function FinalDecision() {
   return (
-    <>
-      <Nav />
-      <section className="result">
-        <ResultVote />
-        <FinalContent />
-      </section>
-    </>
+    <section className="result">
+      <ResultVote />
+      <FinalContent />
+    </section>
   );
 }
