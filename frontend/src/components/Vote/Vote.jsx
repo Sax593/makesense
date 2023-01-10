66ec54374e4d7dfdb6999c8352d227654b688691
guenk <gueguenk@gmail.com>
2023-01-10 17:26:11 +0100

    p instead of textarea

--- frontend/src/components/Vote/Vote.jsx
+++ frontend/src/components/Vote/Vote.jsx
@@ -2,26 +2,30 @@ import "./style.scss";
 
 export default function Vote() {
   return (
-    <div className="vote">
-      <label>
+    <div className="vote-container">
+      <div className="vote center-items">
         <h2 className="proptitle">Première proposition</h2>
-        <textarea id="fpro1" name="fpro" rows="15" />
+        <p id="fpro1" name="fpro" />
         <button className="vote-button" type="button">
           Voter
         </button>
+      </div>
+
+      <div className="vote center-items">
         <h2 className="proptitle">Deuxième proposition</h2>
-
-        <textarea id="fpro2" name="fpro" rows="15" />
+        <p id="fpro2" name="fpro" />
         <button className="vote-button" type="button">
           Voter
         </button>
+      </div>
+
+      <div className="vote center-items">
         <h2 className="proptitle">Troisième proposition</h2>
-
-        <textarea id="fpro3" name="fpro" rows="15" />
+        <p id="fpro3" name="fpro" />
         <button className="vote-button" type="button">
           Voter
         </button>
-      </label>
+      </div>
     </div>
   );
 }
