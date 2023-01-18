e1629fe0fc7aeac18750d6acfde3c7e06a0f81ec
Alexis Foulont <alexisfoulont@gmail.com>
2023-01-18 12:08:57 +0100

    layout and design of main suggest page + removed duplicate route from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
 import Home from "@pages/Home/Home";
-import SubscriptionForm from "@pages/SubscriptionForm/SubscriptionForm";
 import Thread from "@pages/Thread/Thread";
 import Register from "@pages/Register/Register";
 import { Routes, Route } from "react-router-dom";
 import SuggestForm from "@pages/SuggestForm/SuggestForm";
 import FinalDecision from "@components/FinalDecision/FinalDecision";
+import Suggest from "@pages/Suggest/Suggest";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<SubscriptionForm />} />
         <Route path="/thread" element={<Thread />} />
         <Route path="/register" element={<Register />} />
         <Route path="/suggestform" element={<SuggestForm />} />
         <Route path="/finaldecision" element={<FinalDecision />} />
-
+        <Route path="/suggestionDetails" element={<Suggest />} />
       </Routes>
     </div>
   );
