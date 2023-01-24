cf6243a2c089f1673993ea62eb4c9a6a966d9420
sax59 <waquetv@gmail.com>
2023-01-24 17:10:30 +0100

    Create progressbar components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import { Routes, Route } from "react-router-dom";
 import SuggestForm from "@pages/SuggestForm/SuggestForm";
 import FinalDecision from "@components/FinalDecision/FinalDecision";
 import Vote from "@components/Vote/Vote";
-import SuggestDetails from "@components/SuggestDetails/SuggestDetails";
 import Suggest from "@pages/Suggest/Suggest";
 
 function App() {
