021fd24eaf6e0cdb0f36e79571c476bf9916fc59
Alexis Foulont <alexisfoulont@gmail.com>
2023-01-25 10:29:19 +0100

    responsive design home page

--- frontend/src/components/SugestCard/SugestCard.jsx
+++ frontend/src/components/SugestCard/SugestCard.jsx
@@ -1,24 +1,20 @@
-import { IoIosAddCircleOutline } from "react-icons/io";
 import PropTypes from "prop-types";
 import "./style.scss";
 import { Link } from "react-router-dom";
 
 export default function SuggestCard({ data }) {
   return (
-    <article className="cardz">
-      <h2>{data.title}</h2>
-      <div className="wrapper">
-        <div className="progressBar" />
-      </div>
-      <div className="footerCard">
-        <p className="advice">Avis(5)</p>
-        <Link to={`/suggestionDetails/${data.id}`}>
-          <button className="adviceButton" type="button">
-            <IoIosAddCircleOutline />
-          </button>
-        </Link>
-      </div>
-    </article>
+    <Link to={`/suggestionDetails/${data.id}`} className="suggestBlock">
+      <article className="cardz">
+        <h2 className="titleCard">{data.title} </h2>
+        <div className="wrapper">
+          <div className="progressBar" />
+        </div>
+        <div className="footerCard">
+          <p className="opinion">Opinions(5)</p>
+        </div>
+      </article>
+    </Link>
   );
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,9 +3,6 @@ import axios from "axios";
 import SugestCard from "@components/SugestCard/SugestCard";
 import Nav from "@components/Nav/Nav";
 import "./Style.scss";
-import logoMS from "../../assets/pills.svg";
-import logoMS2 from "../../assets/pills2.svg";
-import logoMS3 from "../../assets/pills3.svg";
 
 export default function Home() {
   const [suggest, setSuggest] = useState([]);
@@ -19,36 +16,65 @@ export default function Home() {
   return (
     <>
       <Nav />
-
-      <img className="logoxMS" src={logoMS} alt="logoMS" />
-      <img className="logoxMS2" src={logoMS2} alt="logoMS" />
-      <img className="logoxMS3" src={logoMS3} alt="logoMS" />
-
-      <section className="titlehome">
-        <h1 className="settingstitle"> </h1>
+      <header id="headerSuggestion">
         <h2 className="titlesuggest">Suggestions</h2>
-        <fieldset className="filteridea">
-          <legend>Priorisation </legend>
-
-          <div>
-            <input type="checkbox" id="high" name="high" />
-            <label htmlFor="high">Haute</label>
+      </header>
+      <section className="titlehome">
+        <div className="filterBlock">
+          <fieldset className="filteridea">
+            <legend className="priorisation">Priorisation </legend>
+            <div>
+              <input
+                className="checkboxP"
+                type="checkbox"
+                id="none"
+                name="none"
+              />
+              <label className="priorityLabel noneP" htmlFor="low">
+                None
+              </label>
+            </div>
+            <div>
+              <input
+                className="checkboxP"
+                type="checkbox"
+                id="low"
+                name="low"
+              />
+              <label className="priorityLabel lowP" htmlFor="low">
+                Low
+              </label>
+            </div>
+            <div>
+              <input
+                className="checkboxP"
+                type="checkbox"
+                id="mid"
+                name="mid"
+              />
+              <label className="priorityLabel mediumP " htmlFor="mid">
+                Medium
+              </label>
+            </div>
+            <div>
+              <input
+                className="checkboxP"
+                type="checkbox"
+                id="high"
+                name="high"
+              />
+              <label className="priorityLabel hightP" htmlFor="high">
+                High
+              </label>
+            </div>
+          </fieldset>
+        </div>
+        <div className="cardsBlock">
+          <div className="cardx">
+            {suggest.map((element) => {
+              return <SugestCard key={element.id} data={element} />;
+            })}
           </div>
-
-          <div>
-            <input type="checkbox" id="mid" name="mid" />
-            <label htmlFor="mid">Moyenne</label>
-          </div>
-
-          <div>
-            <input type="checkbox" id="low" name="low" />
-            <label htmlFor="low">Basse</label>
-          </div>
-        </fieldset>
-        <div className="cardx">
-          {suggest.map((element) => {
-            return <SugestCard key={element.id} data={element} />;
-          })}
         </div>
       </section>
     </>
