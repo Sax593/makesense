5cb352fc310f86fe3aaacb2334db2b87cdbfdd39
Alexis Foulont <alexisfoulont@gmail.com>
2023-01-25 12:12:55 +0100

    insert date deadline block + link to bdd

--- frontend/src/pages/SuggestForm/SuggestForm.jsx
+++ frontend/src/pages/SuggestForm/SuggestForm.jsx
@@ -1,3 +1,4 @@
+import Nav from "@components/Nav/Nav";
 import axios from "axios";
 import { useState } from "react";
 import { IoIosPersonAdd } from "react-icons/io";
@@ -6,12 +7,16 @@ import "./style.scss";
 export default function SuggestForm() {
   const [search, setSearch] = useState(true);
   const [visibility, setVisibility] = useState(true);
+  const [date, setDate] = useState(true);
 
   const [suggest, setSuggest] = useState({
     title: "",
     description: "",
     consequences: "",
     priority: 0,
+    contribution_date: "",
+    vote_date: "",
+    final_date: "",
   });
 
   const hChange = (evt) => {
@@ -28,94 +33,141 @@ export default function SuggestForm() {
 
   return (
     <section className="form_idea">
-      <p className="suggFormTitle">Suggestion Form</p>
+      <Nav />
+      <header className="headerSuggestF">
+        <p className="suggFormTitle">Suggestion Form</p>
+      </header>
       <form className="suggForm" onSubmit={hSubmit}>
         <div className="inputBlock">
           <div className="left">
             <label className="labelTitle" htmlFor="title">
               Title:
+              <input
+                className="inputText"
+                type="text"
+                name="title"
+                id="title"
+                value={suggest.title}
+                onChange={hChange}
+              />
             </label>
-            <input
-              className="inputText"
-              type="text"
-              name="title"
-              id="title"
-              value={suggest.title}
-              onChange={hChange}
-            />
             <label className="labelDescription" htmlFor="description">
               Description:
+              <textarea
+                className="textArea"
+                name="description"
+                id="description"
+                cols="30"
+                rows="10"
+                value={suggest.description}
+                onChange={hChange}
+              />
             </label>
-            <textarea
-              className="textArea"
-              name="description"
-              id="description"
-              cols="30"
-              rows="10"
-              value={suggest.description}
-              onChange={hChange}
-            />
             <label className="labelRepercussion" htmlFor="impact">
               Potential repercution:
+              <textarea
+                className="textArea"
+                name="consequences"
+                id="consequences"
+                cols="30"
+                rows="10"
+                value={suggest.consequences}
+                onChange={(evt) => {
+                  setSuggest({ ...suggest, consequences: evt.target.value });
+                }}
+              />
             </label>
-            <textarea
-              className="textArea"
-              name="consequences"
-              id="consequences"
-              cols="30"
-              rows="10"
-              value={suggest.consequences}
-              onChange={(evt) => {
-                setSuggest({ ...suggest, consequences: evt.target.value });
-              }}
-            />
           </div>
           <div className="right">
-            <label className="vote" htmlFor="vote">
-              Would you like to add some votes?
-              <input type="checkbox" name="vote" id="vote" />
-            </label>
-            <label className="labelImpacted add_button" htmlFor="impacted">
-              Impacted:
-            </label>
-            <div className="impactedBlock">
-              <button
-                onClick={() => {
-                  setSearch(!search);
-                }}
-                className="add"
-                type="button"
-              >
-                <IoIosPersonAdd className="personAdd" />
-              </button>
-            </div>
-            <input
-              className={search ? "search" : null}
-              type="search"
-              placeholder="name"
-              onChange={hChange}
-            />
+            <div className="ExpImpBlock">
+              <label className="vote" htmlFor="vote">
+                Would you like to add some votes?
+                <input
+                  type="checkbox"
+                  name="vote"
+                  id="vote"
+                  onClick={() => {
+                    setDate(!date);
+                  }}
+                />
+              </label>
+              <label className="labelImpacted add_button" htmlFor="impacted">
+                Impacted:
+              </label>
+              <div className="impactedBlock">
+                <button
+                  onClick={() => {
+                    setSearch(!search);
+                  }}
+                  className="add"
+                  type="button"
+                >
+                  <IoIosPersonAdd className="personAdd" />
+                </button>
+              </div>
+              <input
+                className={search ? "search" : null}
+                type="search"
+                placeholder="name"
+                onChange={hChange}
+              />
 
-            <label className="labelExpert add_button" htmlFor="expert">
-              Experts:
-            </label>
-            <div className="expertBlock">
-              <button
-                className="add"
-                type="button"
-                onClick={() => {
-                  setVisibility(!visibility);
-                }}
-              >
-                <IoIosPersonAdd className="personAdd" />
-              </button>
+              <label className="labelExpert add_button" htmlFor="expert">
+                Experts:
+              </label>
+              <div className="expertBlock">
+                <button
+                  className="add"
+                  type="button"
+                  onClick={() => {
+                    setVisibility(!visibility);
+                  }}
+                >
+                  <IoIosPersonAdd className="personAdd" />
+                </button>
+              </div>
+              <input
+                className={visibility ? "search" : null}
+                type="search"
+                placeholder="name"
+                onChange={hChange}
+              />
             </div>
-            <input
-              className={visibility ? "search" : null}
-              type="search"
-              placeholder="name"
-              onChange={hChange}
-            />
+            <div id="datesBlock" className={date ? "hide" : null}>
+              <div className="contribDateBlock">
+                <label className="contribLabl">
+                  Contributions deadline:
+                  <input
+                    className="contribDate"
+                    type="date"
+                    name="contribution_date"
+                    value={suggest.contribution_date}
+                  />
+                </label>
+              </div>
+              <div className="voteDateBlock">
+                <label className="voteLabl">
+                  Votes deadline:
+                  <input
+                    className="voteDate"
+                    type="date"
+                    name="vote_date"
+                    value={suggest.vote_date}
+                  />
+                </label>
+              </div>
+              <div className="finaleDateBlock">
+                <label className="finaleLabl">
+                  Decision deadline:
+                  <input
+                    className="finaleDate"
+                    type="date"
+                    name="final_date"
+                    value={suggest.final_date}
+                  />
+                </label>
+              </div>
+            </div>
           </div>
         </div>
         <button className="sendButton" type="submit">
