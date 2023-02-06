2593ca483018515c8f4e3785d0ac3028e1d0565d
guenk <gueguenk@gmail.com>
2023-02-06 15:17:59 +0100

    remove border fontsize

--- frontend/src/components/FinalDecision/FinalDecision.jsx
+++ frontend/src/components/FinalDecision/FinalDecision.jsx
@@ -1,19 +1,15 @@
 import FinalContent from "@components/FinalContent/FinalContent";
 import ResultVote from "@components/ResultVote/ResultVote";
 import propTypes from "prop-types";
-import line from "../../assets/line.svg";
 
 import "./style.scss";
 
 export default function FinalDecision({ suggestData }) {
   return (
-    <>
-      <section className="result">
-        <ResultVote />
-        <FinalContent suggest={suggestData} />
-      </section>
-      <img className="linefinal" src={line} alt="line" />
-    </>
+    <section className="result">
+      <ResultVote />
+      <FinalContent suggest={suggestData} />
+    </section>
   );
 }
 
